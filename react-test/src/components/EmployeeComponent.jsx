c94db0725b204fa38972ae90a0f255cc1d9b0745
myname <yx18627792049@gmail.com>
2024-11-30 16:11:32 +0900

    输入框验证添加

--- react-test/src/components/EmployeeComponent.jsx
+++ react-test/src/components/EmployeeComponent.jsx
@@ -8,18 +8,48 @@ const EmployeeComponent = () => {
   const[email,setEmail]=useState('')
   const navigator=useNavigate();
 
+  const [errors,setErrors]=useState({
+    name:"",
+    email :""
+  })
+
    const saveEmployee = (e) => {
       e.preventDefault();
-      const employee={name,email};
-      console.info(employee);
-      insertEmployee(employee).then((response)=>{
-        console.log(response.data);
-        navigator('/employee');
-      })
+     if (validateForm()) {
+       const employee = { name, email };
+       console.info(employee);
+       insertEmployee(employee).then((response) => {
+         console.log(response.data);
+         navigator('/employee');
+       })
+     }else{
+
+     }
+      
   }
   const cannelEmployee = () => {
     navigator('/employee')
   }
+
+  function validateForm(){
+    let valid = true;
+    const errorCopy={...errors}
+    if(name.trim()){
+      errorCopy.name="";
+    }else{
+      errorCopy.name="name is requird";
+      valid=false;
+    }
+    if(email.trim()){
+      errorCopy.email="";
+    }else{
+      errorCopy.email="email is requird";
+      valid=false;
+    }
+    setErrors(errorCopy);
+    return valid;
+  }
+
   return (
     <div className='container'>
       <div className='row'>
@@ -29,11 +59,13 @@ const EmployeeComponent = () => {
             <form>
               <div className='form-group mb-2'>
                 <label className='form-label'>Name:</label>
-                <input type="text" className="form-control" placeholder="Enter Employee Name" name="name" value={name} onChange={(e)=>setName(e.target.value)}></input>
+                <input type="text" className={`form-control ${errors.name? 'is-invalid':''}`} placeholder="Enter Employee Name" name="name" value={name} onChange={(e)=>setName(e.target.value)}></input>
+                {errors.name && <div className='invalid-feedback'>{errors.name}</div>}
               </div>
               <div className='form-group mb-2'>
                 <label className='form-label'>Email:</label>
-                <input type="text" className="form-control" placeholder="Enter Employee Email" name="email" value={email} onChange={(e)=>setEmail(e.target.value)}></input>
+                <input type="text" className={`form-control ${errors.email? 'is-invalid':''}`} placeholder="Enter Employee Email" name="email" value={email} onChange={(e)=>setEmail(e.target.value)}></input>
+                {errors.email && <div className='invalid-feedback'>{errors.email}</div>}
               </div>
               <div className='text-center'>
                 <button className='btn btn-success' onClick={saveEmployee}>submit</button>
